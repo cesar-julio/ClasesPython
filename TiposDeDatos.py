133e4e0ab823f43547b893fcf18adff311f96950
cesar-julio <juliocesarlopezzelada@gmail.com>
2020-11-22 21:46:05 -0500

    Más agregado

---
+++ TiposDeDatos.py
@@ -0,0 +1,21 @@
+print("¿Qué son los tipos de tados?\n Los datos pueden ser un   TEXTO, NÚMERO o cualquier otra cosa intermedia")
+print("")
+print("TIPOS DE DATOS:\n CADENAS .- Cualquier valor de texto que podamos usar, como nombres, frases, todo se denomina cadena. \nEs una colección de caracteres")
+cadena = "Soy una cadena"
+print(cadena)#Modelo de variable
+print(">> NOTA:Las se usan los dos tipos de comillas tanto simples como compuestas para poder incluir una dentro de otra.")
+print("")
+print("DATOS NÚMERICOS.- Esta compuesto por los números ENTEROS y DECIMALES")
+Numero_en = 12 #Modelo de variable
+Numero_de = 0.23 #Modelo de variable
+print(str(Numero_de) + " Soy entero ," + str(Numero_de) + " Soy decimal")
+print(">> NOTA: Los desimales se utilizan para tener una mayor precicón a la ora de hacer calculos ")
+print("")
+print("DATOS BOOLEANS.- Admite dos valores VERDADERO o FALSO ")
+Val_verdadero = True #Modelo de variable
+Val_falso = False #Modelo de variable
+print(str(Val_verdadero) + " soy verdadero, " + str(Val_falso) +" soy falso")
+print(">> NOTA: Su uso es fundamental para la toma de deciciones dentro de un programa")
+print("LISTAS .- Enumera objetos, valores o almacena tatos entre corchetes")
+mes = ['Enero', 'Febrero', 'Marzo'] #Modelo de variable
+print(mes)
---
+++ test02.py
@@ -0,0 +1,2 @@
+print("I'm afraid I won't be able toi make it")
+print('HE SAID"wHY DO YOU CARE"')
