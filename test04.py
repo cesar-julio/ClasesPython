25eca7bab93af79580d9a9ee26d895f5cc823c29
cesar-julio <juliocesarlopezzelada@gmail.com>
2020-11-22 22:53:30 -0500

    Agreagado del test04

---
+++ test04.py
@@ -0,0 +1,7 @@
+print("Hola y bienbenido a mi tutorial interactivo.")
+nombre = input("Tu nombre es: ")
+edad = int(input("Tu edad es: "))
+ciudad = input("¿Dónde vives?: ")
+email = input("Por favor, introduzca su dirección de correo electrónico: ")
+
+print(f"Muchas gracias {nombre}, se le contactará en {email}.")
